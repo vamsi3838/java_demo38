c0431af5888f74ee3f722fcfbf803ca58c83eca4
vamsi3838 <konetivamsikumar@gmail.com>
2024-11-01 22:11:04 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+class Factorial { 
+	
+	static int factorial(int n) 
+	{ 
+		int res = 1, i; 
+		for (i = 2; i <= n; i++) 
+			res *= i; 
+		return res; 
+	} 
+
+	// main method 
+	public static void main(String[] args) 
+	{ 
+		int num = 5; 
+		System.out.println("Factorial of " + num + " is "
+						+ factorial(5)); 
+	} 
+}
